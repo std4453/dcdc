60e50fc3b879330bec6b916c261f959255e0d718
Haichuan Zhang <me@std4453.com>
2020-04-26 00:13:25 +0800

    Cleanup code

--- index.js
+++ index.js
@@ -18,16 +18,14 @@ const init = () => {
     canvas.width = width;
     canvas.height = height;
     canvas.style.transform = `scale(${1 / window.devicePixelRatio})`;
-    canvas.style.transformOrigin = 'left top';
     container.appendChild(canvas);
+    const ctx = canvas.getContext('2d');
 
-    let redraw, seed;
-    const invalidate = () => { redraw = true; };
+    const invalidate = () => { rerender(); };
+    let seed;
     const reseed = () => {
         seed = `${new Date().getTime()}`;
     }
-    invalidate();
-    reseed();
     canvas.addEventListener('click', () => {
         reseed();
         invalidate();
@@ -39,48 +37,43 @@ const init = () => {
         }
     });
 
-    const ctx = canvas.getContext('2d');
-
     const options = {
-        fps: 10,
         mode: modes[0],
         background: '#F2F2F2',
     };
-
     const gui = new dat.GUI();
     gui.add(options, 'mode', modes).onChange(invalidate);
     gui.addColor(options, 'background').onChange(invalidate);
 
+    // initialize modules
     const inflated = {};
     for (const key in modules) {
         inflated[key] = modules[key]({ gui, ctx, invalidate, width, height, inflated });
     }
 
-    const frame = () => {
-        if (redraw) {
-            redraw = false;
-            seedrandom(seed, { global: true });
-            ctx.fillStyle = Color(options.background).string();
-            ctx.fillRect(0, 0, width, height);
-            const text = choose(
-                '被害妄想携帯女子',
-                'ジェットブーツで大気圏を突破して',
-                '短気呑気男子電気消さないで',
-                'ただいま参上！電波シスター☆',
-                '千本桜　夜ニ紛レ',
-                'にゃん　にゃん　にゃん　ステップ踏んで',
-                '太陽曰く燃えよカオス',
-                'インターネットが遅いさん',
-                'ごめん　ゆずれない　ゆずれない',
-            );
-            const render = inflated[options.mode].fn;
-            modes.map(key => inflated[key].folder).forEach(f => f.hide());
-            inflated[options.mode].folder.show();
-            render(text, options);
-        }
-        requestAnimationFrame(frame);
-    }
-    frame();
+    const rerender = () => {
+        seedrandom(seed, { global: true });
+        ctx.fillStyle = Color(options.background).string();
+        ctx.fillRect(0, 0, width, height);
+        const text = choose(
+            '被害妄想携帯女子',
+            'ジェットブーツで大気圏を突破して',
+            '短気呑気男子電気消さないで',
+            'ただいま参上！電波シスター☆',
+            '千本桜　夜ニ紛レ',
+            'にゃん　にゃん　にゃん　ステップ踏んで',
+            '太陽曰く燃えよカオス',
+            'インターネットが遅いさん',
+            'ごめん　ゆずれない　ゆずれない',
+        );
+        const render = inflated[options.mode].fn;
+        modes.map(key => inflated[key].folder).forEach(f => f.hide());
+        inflated[options.mode].folder.show();
+        render(text, options);
+    };
+
+    invalidate();
+    reseed();
 }
 
 init();
