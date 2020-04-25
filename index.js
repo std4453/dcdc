b0cf7d04c49808634e85c65638e589f8ba676105
Haichuan Zhang <me@std4453.com>
2020-04-26 00:43:00 +0800

    Extrace example text

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ import { choose } from './utils';
 const width = 1920, height = 1080;
 const modules = { drawText, split, balanced, neutral, activeA };
 const modes = ['balanced', 'neutral', 'activeA'];
+const exampleText = [
+    '被害妄想携帯女子',
+    'ジェットブーツで大気圏を突破して',
+    '短気呑気男子電気消さないで',
+    'ただいま参上！電波シスター☆',
+    '千本桜　夜ニ紛レ',
+    'にゃん　にゃん　にゃん　ステップ踏んで',
+    '太陽曰く燃えよカオス',
+    'インターネットが遅いさん',
+    'ごめん　ゆずれない　ゆずれない',
+];
 
 const init = () => {
     const container = document.querySelector('#container');
@@ -57,17 +68,7 @@ const init = () => {
         seedrandom(seed, { global: true });
         ctx.fillStyle = Color(options.background).string();
         ctx.fillRect(0, 0, width, height);
-        const text = options.text !== '' ? options.text : choose(
-            '被害妄想携帯女子',
-            'ジェットブーツで大気圏を突破して',
-            '短気呑気男子電気消さないで',
-            'ただいま参上！電波シスター☆',
-            '千本桜　夜ニ紛レ',
-            'にゃん　にゃん　にゃん　ステップ踏んで',
-            '太陽曰く燃えよカオス',
-            'インターネットが遅いさん',
-            'ごめん　ゆずれない　ゆずれない',
-        );
+        const text = options.text !== '' ? options.text : choose(...exampleText);
         const render = inflated[options.mode].fn;
         modes.map(key => inflated[key].folder).forEach(f => f.hide());
         inflated[options.mode].folder.show();
