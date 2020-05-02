e341dc71fdcd93d9da5cf5ba96d06dcd20b928a9
Haichuan Zhang <me@std4453.com>
2020-05-02 23:12:00 +0800

    Add autosave

--- retex/index.js
+++ retex/index.js
@@ -5,22 +5,40 @@ import ContextMenuPlugin from 'rete-context-menu-plugin';
 import MyNode from './MyNode';
 import components from "../components";
 
-export default () => {
+export default async () => {
     const editor = new Rete.NodeEditor('dcdc@0.1.0', document.querySelector('#rete'));
     editor.use(ConnectionPlugin);
     editor.use(ReactRenderPlugin, { component: MyNode });
     editor.use(ContextMenuPlugin);
-
+    
     const engine = new Rete.Engine('dcdc@0.1.0');
-
+    
     components.forEach(component => {
         const instance = new component();
         editor.register(instance);
         engine.register(instance);
     });
+    
+    if (localStorage['retex']) {
+        try {
+            const data = JSON.parse(localStorage['retex']);
+            await editor.fromJSON(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    
+    let lastData = null;
 
     editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
         await engine.abort();
-        await engine.process(editor.toJSON());
+        const data = await editor.toJSON();
+        lastData = data;
+        await engine.process(data);
+        setTimeout(() => {
+            if (data !== lastData) return;
+            localStorage['retex'] = JSON.stringify(data);
+            console.log('Data saved!');
+        }, 3000);
     });
 }
