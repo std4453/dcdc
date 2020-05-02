ee3442c0681feab72987c0e6c74ff1492bc5c684
Haichuan Zhang <me@std4453.com>
2020-05-02 20:17:12 +0800

    Extract init function

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
 import '@babel/polyfill';
+import init from './retex';
 
-import Rete from 'rete';
-import ConnectionPlugin from 'rete-connection-plugin';
-import ReactRenderPlugin from 'rete-react-render-plugin';
-import ContextMenuPlugin from 'rete-context-menu-plugin';
-import components from "./components";
-
-const editor = new Rete.NodeEditor('dcdc@0.1.0', document.querySelector('#rete'));
-editor.use(ConnectionPlugin);
-editor.use(ReactRenderPlugin);
-editor.use(ContextMenuPlugin);
-
-const engine = new Rete.Engine('dcdc@0.1.0');
-
-components.forEach(component => {
-    const instance = new component();
-    editor.register(instance);
-    engine.register(instance);
-});
-
-editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
-    await engine.abort();
-    await engine.process(editor.toJSON());
-});
+init();
