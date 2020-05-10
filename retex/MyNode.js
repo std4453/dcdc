7e6c2097103c745464dc9d0eddcfc2b09835f5f7
Haichuan Zhang <me@std4453.com>
2020-05-10 17:30:49 +0800

    Simple adaptation of react-dat-gui

--- retex/MyNode.js
+++ retex/MyNode.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import { Node, Socket, Control } from 'rete-react-render-plugin';
 import * as _ from 'lodash';
-import ControlPanel, {
-    Checkbox, Text, Color, Range, Select,
-} from 'react-control-panel';
+import DatGui, {
+    DatBoolean, DatColor, DatNumber, DatString, DatPresets, DatFolder
+} from 'react-dat-gui';
 import typeName from 'type-name';
+import 'react-dat-gui/dist/index.css';
 import './styles.css';
 
 function Label({ name, align = 'left' }) {
     return (
-        <div className="container">
-            <div style={{ textAlign: align }}>
-                <span style={{ verticalAlign: 'sub', color: 'rgb(235, 235, 235) '}}>
+        <li className="cr">
+            <label style={{ justifyContent: { left: 'flex-start', right: 'flex-end' }[align] }}>
+                <span style={{ lineHeight: '27px' }}>
                     {name}
                 </span>
-            </div>
-        </div>
+            </label>
+        </li>
     );
 }
 
 function DefaultInput({ name, inputDef: { type, defaultVal, options, controlType }, inputDef, io }) {
-    if (io.connections.length !== 0) return <Label name={name}/>
+    if (io.connections.length !== 0) return <Label name={name} />
 
     if (!type) type = typeName(defaultVal);
     if (!controlType) controlType = type;
-    
-    if (controlType === 'string') return <Text label={name}/>
-    if (controlType === 'boolean') return <Checkbox label={name}/>
+
+    if (controlType === 'string') return <DatString path={name} label={name} />
+    if (controlType === 'boolean') return <DatBoolean path={name} label={name} />
     if (controlType === 'number') {
         const { min, max, step = 0.01 } = inputDef;
-        return <Range label={name} min={min} max={max} step={step}/>
+        return <DatNumber path={name} label={name} min={min} max={max} step={step} />
     }
-    if (controlType === 'color') return <Color label={name} format="rgb"/>
-    if (controlType === 'select') return <Select label={name} options={options}/>
-    return <Label name={name}/>
+    if (controlType === 'color') return <DatColor label={name} format="rgb" />
+    if (controlType === 'select') return <DatPresets label={name} options={options} />
+    return <Label name={name} />
 }
 
 class MyNode extends Node {
@@ -44,23 +45,28 @@ class MyNode extends Node {
         const initialState = _.fromPairs(_
             .toPairs(node.inputDefs)
             .map(([name, { defaultVal }]) => [name, defaultVal]));
+        console.log(initialState);
 
         return (
-            <ControlPanel
-                theme="dark"
-                title={node.name}
-                initialState={initialState}
-                onChange={(key, value) => {
-                    const { convert } = node.inputDefs[key];
-                    node.data[key] = convert ? convert(value) : value;
+            <DatGui
+                data={initialState}
+                onUpdate={(newData) => {
+                    newData = { ...newData, ...node.data };
+                    for (const key in newData) {
+                        const { convert } = node.inputDefs[key];
+                        if (convert) newData[key] = convert(newData[key]);
+                    }
+                    node.data = newData;
                     editor.trigger('process');
                 }}
-                width={300}
-                style={{ marginRight: 30 }}
-            > 
+                style={{
+                    position: 'relative',
+                    backgroundColor: '#1A1A1A',
+                }}
+            >
                 {outputs.map((output) => (
                     <div className="retex-port retex-output" key={output.key}>
-                        <Label name={output.name} align="right"/>
+                        <Label name={output.name} align="right" />
                         <Socket type="output" socket={output.socket} io={output} innerRef={bindSocket} />
                     </div>
                 ))}
@@ -70,11 +76,11 @@ class MyNode extends Node {
                 {inputs.map(input => (
                     <div className="retex-port retex-input" key={input.key} onPointerMove={(e) => { e.stopPropagation(); }}>
                         <Socket type="input" socket={input.socket} io={input} innerRef={bindSocket} />
-                        {!input.showControl() && <DefaultInput name={input.name} inputDef={node.inputDefs[input.name]} io={input}/>}
+                        {!input.showControl() && <DefaultInput name={input.name} inputDef={node.inputDefs[input.name]} io={input} />}
                         {input.showControl() && <Control className="input-control" control={input.control} innerRef={bindControl} />}
                     </div>
                 ))}
-            </ControlPanel>
+            </DatGui>
         )
     }
 }
