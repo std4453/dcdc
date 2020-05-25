674009f9e37dc651515ac1f3a06d9e67ec558ec6
Haichuan Zhang <me@std4453.com>
2020-05-26 01:02:25 +0800

    Move button to NodeMap

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -1,51 +1,16 @@
 import React, { useCallback } from 'react';
-import { makeStyles, Button } from '@material-ui/core';
 import NodeMap from './retex/NodeMap';
 
-const useStyles = makeStyles({
-    button: {
-        position: 'absolute',
-        right: 16,
-        bottom: 16,
-    },
-});
-
 function InitialValues({ setStep }) {
-    const classes = useStyles();
-    const next = useCallback(() => {
-        setStep('GenerationRules');
-    }, [setStep]);
-    return <>
-        <NodeMap saveKey="initial" />
-        <Button
-            variant="contained"
-            color="primary"
-            disableElevation
-            onClick={next}
-            classes={{ root: classes.button }}
-        >
-            下一步
-        </Button>
-    </>;
+    return (
+        <NodeMap setStep={setStep} saveKey="initial" next="GenerationRules"/>
+    );
 }
 
 function GenerationRules({ setStep }) {
-    const classes = useStyles();
-    const next = useCallback(() => {
-        setStep('Generation');
-    }, [setStep]);
-    return <>
-        <NodeMap saveKey="generation" />
-        <Button
-            variant="contained"
-            color="primary"
-            disableElevation
-            onClick={next}
-            classes={{ root: classes.button }}
-        >
-            下一步
-        </Button>
-    </>;
+    return (
+        <NodeMap setStep={setStep} saveKey="generation" next="Generation"/>
+    );
 }
 
 export { InitialValues, GenerationRules };
--- src/retex/NodeMap.jsx
+++ src/retex/NodeMap.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import { makeStyles, Button } from '@material-ui/core';
 import Rete from 'rete';
 import ConnectionPlugin from 'rete-connection-plugin';
 import ReactRenderPlugin from 'rete-react-render-plugin';
@@ -6,7 +7,19 @@ import ContextMenuPlugin from 'rete-context-menu-plugin';
 import MyNode from './MyNode';
 import components from "../components";
 
-function NodeMap({ saveKey }) {
+const useStyles = makeStyles({
+    button: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+    },
+});
+
+function NodeMap({ setStep, saveKey, next }) {
+    const classes = useStyles();
+    const nextStep = useCallback(() => {
+        setStep(next);
+    }, [setStep, next]);
     const [root, setRoot] = useState(null);
     const editor = useMemo(() => {
         if (!root) return null;
@@ -42,7 +55,7 @@ function NodeMap({ saveKey }) {
         })();
     }, [editor, saveKey]);
 
-    return (
+    return <>
         <div ref={setRoot} style={{
             position: 'absolute',
             left: 0,
@@ -50,7 +63,16 @@ function NodeMap({ saveKey }) {
             width: '100vw',
             height: '100vh',
         }}/>
-    )
+        <Button
+            variant="contained"
+            color="primary"
+            disableElevation
+            onClick={nextStep}
+            classes={{ root: classes.button }}
+        >
+            下一步
+        </Button>
+    </>
 }
 
 export default NodeMap;
