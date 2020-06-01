4de46f88c8a379f8ba841206890c5b956379244d
std4453 <std4453@users.noreply.github.com>
2020-06-01 14:10:07 +0000

    Deploying to gh-pages from  @ 304f97510c86d94871ec6850cde4c6cccaa087c7 🚀

---
+++ precache-manifest.ccd7e6736b9233ec546ffcba90ffdb16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af76bdb0970bce8ca09a69c1c9ef826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e44174886ba34f783bf7",
+    "url": "/static/css/2.d38a73a9.chunk.css"
+  },
+  {
+    "revision": "29cafe535a9419b8dbee",
+    "url": "/static/css/main.023f754c.chunk.css"
+  },
+  {
+    "revision": "e44174886ba34f783bf7",
+    "url": "/static/js/2.7b7052b9.chunk.js"
+  },
+  {
+    "revision": "fb92782e390c85d38963e5ff91c726ba",
+    "url": "/static/js/2.7b7052b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cafe535a9419b8dbee",
+    "url": "/static/js/main.570c68c1.chunk.js"
+  },
+  {
+    "revision": "e4d252256119e9e1dff0",
+    "url": "/static/js/runtime-main.d57e1356.js"
+  },
+  {
+    "revision": "9176ce0d2956326bc0f809294ec31f73",
+    "url": "/static/media/logo-colored.9176ce0d.svg"
+  }
+]);
