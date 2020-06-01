a7cac07664f748f3dbdcc114c90e24877e434836
std4453 <me@std4453.com>
2020-06-01 16:16:31 +0000

    Deploying to gh-pages from  @ 8966a1782adbf01a4480624df4c2857b130caabd 🚀

---
+++ precache-manifest.7d2ab75aae488977420299c31788e0ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543235cb4d0ed9f8d4b877325ccb07a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a85f9f2e75ccd336616",
+    "url": "./static/css/2.d38a73a9.chunk.css"
+  },
+  {
+    "revision": "9a6fc64a926530154fc6",
+    "url": "./static/css/main.023f754c.chunk.css"
+  },
+  {
+    "revision": "7a85f9f2e75ccd336616",
+    "url": "./static/js/2.f6929b6a.chunk.js"
+  },
+  {
+    "revision": "fb92782e390c85d38963e5ff91c726ba",
+    "url": "./static/js/2.f6929b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6fc64a926530154fc6",
+    "url": "./static/js/main.7f145be1.chunk.js"
+  },
+  {
+    "revision": "c3a044ce4366385f5c98",
+    "url": "./static/js/runtime-main.549329cc.js"
+  },
+  {
+    "revision": "9176ce0d2956326bc0f809294ec31f73",
+    "url": "./static/media/logo-colored.9176ce0d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.17dbc3e668cc592df812308abaf99265.js"
+  "./precache-manifest.7d2ab75aae488977420299c31788e0ea.js"
 );
 
 self.addEventListener('message', (event) => {
